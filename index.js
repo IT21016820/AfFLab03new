f620e2c486635d9144b4db0503579569c4c6b535
Ashani <ashanidewmini33@gmail.com>
2023-03-01 13:17:37 +0530

    commitReadFile

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Application Frameworks")
+
+//read file
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function(err,data){
+    if(err)throw err;
+    consol.log(data);
+});
